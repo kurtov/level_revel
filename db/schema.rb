c8692f603440769876847bdb3f22bce069922140
kurtov <kurtov@unact.ru>
2018-06-13 00:09:07 +0300

    sqlite -> pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_06_09_180301) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bars", force: :cascade do |t|
     t.string "address"
     t.decimal "latitude"
